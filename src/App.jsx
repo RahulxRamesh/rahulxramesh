e3a0124695b899ff2d7d61461d2ebf57d8c42247
Rahul Ramesh <rahulxramesh@gmail.com>
2022-10-18 15:20:35 -0500

    messing around with padding style paddingY2 values

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,9 @@ const App = () => {
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingY2} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
             <Hero/>
-        </div>
-      </div>
-
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-            <Stats/>
-            <Business/>
-            <Billing/>
-            <CardDeal/>
-            <Testimonials/>
-            <Clients/>
-            <CTA/>
-            <Footer/>
         </div>
       </div>
 
